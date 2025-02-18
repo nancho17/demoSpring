a43ab406953409e13c62fa055613ffd35fc6b7ab
nancho17 <her.dar.ferre@gmail.com>
2025-02-18 04:31:32 -0300

    Rest User implementation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.user.User;
+import com.example.demo.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 
 @SpringBootApplication
@@ -17,8 +23,17 @@ public class DemoApplication {
 	//WelcomeMessage welMeObj = (WelcomeMessage) contextInst.getBean("welcomeMessage");
 	//System.out.println(welMeObj.getWelcomeMEssage());
 
-//	@Bean
-//	CommandLineRunner runner(RunRepository runRepository){
+	@Bean
+	CommandLineRunner runner(UserRestClient client) {
+		return args -> {
+			List<User> users = client.findAll();
+			User user = client.findById(1);
+			System.out.println(user);
+
+		};
+
+	}
+		//	CommandLineRunner runner(RunRepository runRepository){
 //		return args -> {
 //			Run correr = new Run(1,"First RUn runio", LocalDateTime.now(),LocalDateTime.now().plus(10, ChronoUnit.HOURS),5, Location.OUTDOOR);
 //			runRepository.create(correr);
--- src/main/java/com/example/demo/run/RunRepository.java
+++ src/main/java/com/example/demo/run/RunRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface RunRepository extends ListCrudRepository<Run,Integer> {
 
+    @Query
     List<Run> findAllByLocation(String location);
 
 }
