658641f70b283bcb1cceca05935d7c1ce9d13365
nancho17 <her.dar.ferre@gmail.com>
2025-02-18 04:47:04 -0300

    Http client interface

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.user.User;
+import com.example.demo.user.UserHttpClient;
 import com.example.demo.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.util.List;
 
@@ -24,9 +29,19 @@ public class DemoApplication {
 	//System.out.println(welMeObj.getWelcomeMEssage());
 
 	@Bean
-	CommandLineRunner runner(UserRestClient client) {
+	UserHttpClient userHttpClient(){
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
+	}
+
+
+	@Bean
+	CommandLineRunner runner(UserHttpClient client) {
 		return args -> {
 			List<User> users = client.findAll();
+			//System.out.println(users);
+
 			User user = client.findById(1);
 			System.out.println(user);
 
